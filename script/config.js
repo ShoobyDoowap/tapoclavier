1d3a0291e2f689769194db06a750e01e18349a38
ShoobyDoowap <belouxsteph@hotmail.com>
2025-04-05 16:17:49 +0200

    Première Mouture

---
+++ script/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette", "Bonbon"]
+const listePhrases = ["Pas de panique !", "Il était une fois", "Merci pour le poisson"]
